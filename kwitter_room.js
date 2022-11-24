959ea3c7cf9b348ab381dc6944f34c55ad58317c
IbrahimFaisal2028 <108027026+IbrahimFaisal2028@users.noreply.github.com>
2022-11-24 12:47:35 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6H8ulh-fNn5DS-3QIKjtWBIl7EPmulA0",
+    authDomain: "letschat-7d3f1.firebaseapp.com",
+    databaseURL: "https://letschat-7d3f1-default-rtdb.firebaseio.com",
+    projectId: "letschat-7d3f1",
+    storageBucket: "letschat-7d3f1.appspot.com",
+    messagingSenderId: "585129117539",
+    appId: "1:585129117539:web:836cc560d060b78e70f556"
+  };
+  
+  // Initialize Firebase
+   firebase.initializeApp(firebaseConfig);
